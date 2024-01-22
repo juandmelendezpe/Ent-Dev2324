039018f3e5ee7eb895ba3aa844e6f395f65361e2
Juan MP <118199373+juandmelendezpe@users.noreply.github.com>
2024-01-22 12:57:11 +0100

    creacion de nueva clase creacin de un array

---
+++ Entorno_Git/src/main/java/UT3/Entorno_Git/Arrays_mat.java
@@ -0,0 +1,16 @@
+package UT3.Entorno_Git;
+
+public class Arrays_mat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int [] numeros = {2,2,2};
+		
+		for(int i = 0 ;i < numeros.length;i++) {
+			System.out.println("ver array : " + "indice: "+ i+ " valor : "+ numeros[i]+ " -");
+			
+		}
+	}
+
+}
